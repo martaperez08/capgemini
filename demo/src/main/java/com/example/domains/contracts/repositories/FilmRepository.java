13ce6efe8497ddab056451d79a2b0d04430b87d0
martaper <martaper@host.docker.internal>
2023-04-11 13:03:13 +0200

    DemoAUTO

---
+++ demo/src/main/java/com/example/domains/contracts/repositories/FilmRepository.java
@@ -0,0 +1,21 @@
+package com.example.domains.contracts.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.example.domains.core.repositories.contracts.RepositoryWithProjections;
+import com.example.domains.entities.Actor;
+import com.example.domains.entities.Film;
+import com.example.domains.entities.dtos.ActorShort;
+
+
+public interface FilmRepository  extends JpaRepository<Film, Integer>, JpaSpecificationExecutor<Film>, RepositoryWithProjections {
+
+	<T> List<T> findAllBy(Class<T> type);
+	
+
+}
