687f0e8da8bf177e24c3e43f3decedbf96dbc36d
martaperez288 <martaperez288@gmail.com>
2023-04-17 14:27:19 +0200

    operacionesCalcu

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,71 +1,72 @@
-console.log("fffff")
-const numberButtons = document.querySelectorAll('[data-number]')
-const numberOperations = document.querySelectorAll('[data-operation]')
-
-let button= document.querySelector('input[type= button]')
-let result= document.querySelector('output')
+//obtenemos los valores 
+const numberButtons = document.querySelectorAll("[data-number]");
+const numberOperations = document.querySelectorAll("[data-operation]");
+let button = document.querySelector("input[type= button]");
+let result = document.querySelector("output");
+//creamos un array para guardar los numeros
 const numbers = [];
-let signo=true;
-let txt = "";
-    numberButtons.forEach(button => {
-        button.addEventListener('click', ev => {
-             if(signo==true){
-                numbers.push(ev.target.value);
-                signo=false;
-             }
-           
-           
+let txt = ""; // concatenar los numeros
+let tipo = ""; // tipo de operacion
+
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", (ev) => {
+    txt = txt + ev.target.value;
+  });
+});
+
+numberOperations.forEach((button) => {
+  button.addEventListener("click", (ev) => {
+    // vamos añadiendo los numeros en los array
+    // asigamos el tipo 
+    //en el caso igual opramos segun el tipo
+    switch (ev.target.value) {
+      case "+":
+        numbers.push(txt); 
+        txt = "";
+        tipo = "suma";
+        break;
+
+      case "-":
+        numbers.push(txt);
+        txt = "";
+        tipo = "resta";
+        break;
+
+      case "x":
+        numbers.push(txt);
+        txt = "";
+        tipo = "multi";
+        break;
         
-            
+      case "/":
+        numbers.push(txt);
+        txt = "";
+        tipo = "division";
+        break;
 
-
-
-            let i = 0;
-
-            do {
-                console.log(numbers[i]);
-                i++;
-            } while (i < numbers.length);
-             
-            })
-    })
-
-    numberOperations.forEach(button => {
-        button.addEventListener('click', ev => {
-      let resultadoOperacion=0
-            switch (ev.target.value) {
-                case '=':
-                case '+':
-                    signo=true
-                  console.log("suma")
-                  if(numbers.length== 2){
-                    console.log("ddd")
-                 resultadoOperacion= suma(numbers[0],numbers[1])
-                 result.textContent= resultadoOperacion
-                  }
-                  break
-                case '-':
-                    console.log("resta")
-                  break
-                case 'x':
-                    console.log("multi")
-                  break
-                case '/':
-                    console.log("divi")
-                  break
-                  case '=':
-                    console.log("igual")
-                    
-                  break
-                default:
-        
-                  return
-              }
-        })
-    })
-
-    function suma( valorA,  valorB) {
-        return +valorA + +valorB
+      case "=":
+        numbers.push(txt);
+        if (tipo === "suma") result.textContent = suma(numbers[0], numbers[1]);
+        if (tipo === "resta") result.textContent = resta(numbers[0], numbers[1]);
+        if (tipo === "multi") result.textContent = multi(numbers[0], numbers[1]);
+        if (tipo === "division") result.textContent = division(numbers[0], numbers[1]);
+        break;
+      default:
+        return;
     }
+  });
+});
 
-  
+function suma(valorA, valorB) {
+  return +valorA + +valorB;
+}
+function resta(valorA, valorB) {
+  return +valorA - +valorB;
+}
+function multi(valorA, valorB) {
+  return +valorA * +valorB;
+}
+function division(valorA, valorB) {
+  return +valorA / +valorB;
+}
