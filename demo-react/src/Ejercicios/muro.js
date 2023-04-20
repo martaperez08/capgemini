a665e2b3ce163bf391dd82c2286a753ee464cf0a
martaperez288 <martaperez288@gmail.com>
2023-04-20 08:02:05 +0200

    API

--- demo-react/src/Ejercicios/muro.js
+++ demo-react/src/Ejercicios/muro.js
@@ -1,46 +1,60 @@
-
-import React, { Component } from 'react'
-import { ErrorMessage, Esperando } from '../comunes'
+import React, { Component } from "react";
+import { ErrorMessage, Esperando } from "../comunes";
 export default class Muro extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            listado: null,
-            loading: true,
-            error: null
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      listado: null,
+      loading: true,
+      error: null,
+    };
+  }
   render() {
-    if(this.state.loading)
-        return <Esperando />
+    if (this.state.loading) return <Esperando />;
     return (
-        <>
-            {this.state.error && <ErrorMessage msg={this.state.error} />}
-            <h1>Muro</h1>
-            {JSON.stringify(this.state.listado)}
-        </>
-    )
+      <>
+        {this.state.error && <ErrorMessage msg={this.state.error} />}
+        <h1>Muro</h1>
+
+        {this.state.listado &&
+          this.state.listado.map((data, key) => {
+            return (
+              <div className="card" style={{ width: "18rem" }}>
+                <img className="card-img-top" src="{{data.download_url}}" alt="Card image cap" />
+                <div className="card-body">
+                  <h5 className="card-title">{data.author}</h5>
+                  <p className="card-text">
+                    
+                  </p>
+                  <a href="#" className="btn btn-primary">
+                    Go somewhere
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+           {JSON.stringify(this.state.listado)}
+      </>
+    );
   }
 
   setError(msg) {
-    this.setState({error: msg})
+    this.setState({ error: msg });
   }
   load(num) {
-    this.setState({loading: true})
-    fetch('https://picsum.photos/v2/list')
-        .then(resp => {
-            if(resp.ok) {
-                resp.json().then(
-                    data => this.setState({listado: data})
-                )
-            } else {
-                this.setError(resp.status)
-            }
-        })
-        .catch(err => this.setError(JSON.stringify(err)))
-        .finally(() => this.setState({loading: false}))
+    this.setState({ loading: true });
+    fetch("https://picsum.photos/v2/list")
+      .then((resp) => {
+        if (resp.ok) {
+          resp.json().then((data) => this.setState({ listado: data }));
+        } else {
+          this.setError(resp.status);
+        }
+      })
+      .catch((err) => this.setError(JSON.stringify(err)))
+      .finally(() => this.setState({ loading: false }));
   }
   componentDidMount() {
-    this.load(1)
+    this.load(1);
   }
 }
